4355b5ccf095cee6f47f1f7e2dc9d9bc00ea4e61
x1aoren <xiaorenflightkit@foxmail.com>
2025-11-01 17:04:00 +0800

    chore: 更新构建号至20251101.2

--- Utils/VersionInfo.cs
+++ Utils/VersionInfo.cs
@@ -20,7 +20,7 @@ namespace ObsMCLauncher.Utils
         /// 格式：日期.构建次数 (yyyyMMdd.build)
         /// 示例：20250108.1, 20250108.2
         /// </summary>
-        public static readonly string BuildVersion = "20251101.1";
+        public static readonly string BuildVersion = "20251101.2";
 
         /// <summary>
         /// 完整版本号（组合显示）
